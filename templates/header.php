cd706fc16b626830325ad111ba9616222d8d2560
will-felipe-souza <will.felipe.souza@gmail.com>
2023-02-19 10:58:23 -0300

    Criado o Header

---
+++ templates/header.php
@@ -0,0 +1,22 @@
+<?php
+  include_once("helpers/url.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog Curso</title>
+    <!-- Estilos do projetos -->
+    <link rel="stylesheet" href="<?= $BASE_URL ?>/css/styles.css">
+    <!-- Fontes -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap" rel="stylesheet">
+  </head>
+  <body>
+    <header>
+      Template cabeçalho
+    </header>
