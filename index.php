af2cb23d15620faa72a9eddbc689eae40264ba8c
will-felipe-souza <will.felipe.souza@gmail.com>
2023-02-19 10:58:48 -0300

    Criado o Index

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+  include_once("helpers/url.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<?php
+  include_once("templates/header.php");
+?>
+
+  <h1>Olá Mundo!</h1>
+
+<?php
+  include_once("templates/footer.php");
+?>
